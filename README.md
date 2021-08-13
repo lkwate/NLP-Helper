2ab575c1ffe6cd146c61722722c85622822a0612
lkwate <kwatedassiloic@gmail.com>
2021-08-13 21:33:05 +0200

    n-gram mlm collator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NLP-Helper
+set of utility tools for NLP tasks.
+
+## N-gram Masked Language Modeling Collator
+    A N-gram MLM collator compatible with a torch DataLoader
+```python
+from utils.mlm_n_gram_collator import NGramDataCollatorForLanguageModeling
+from torch.utils.data import DataLoader
+
+collator = NGramDataCollatorForLanguageModeling(tokenizer=tokenizer, n_gram=3, mlm_probality=.15)
+dataloader = DataLoader(dataset, collate_fn=collator)
+```
---
+++ utils/mlm_n_gram_collator.py
@@ -0,0 +1,88 @@
+"""N-gram DataCollator compatible with huggingface"""
+from dataclasses import dataclass
+import torch
+from typing import Optional, List, Union, Dict, Tuple
+from transformers import PreTrainedTokenizerBase, BatchEncoding, DataCollatorForLanguageModeling
+from torch.utils.data import DataLoader
+
+
+@dataclass
+class NGramDataCollatorForLanguageModeling(DataCollatorForLanguageModeling):
+    """
+    Data collator used for language modeling. Inputs are dynamically padded to the maximum length of a batch if they
+    are not all of the same length.
+
+    Args:
+        tokenizer (:class:`~transformers.PreTrainedTokenizer` or :class:`~transformers.PreTrainedTokenizerFast`):
+            The tokenizer used for encoding the data.
+        mlm (:obj:`bool`, `optional`, defaults to :obj:`True`):
+            Whether or not to use masked language modeling. If set to :obj:`False`, the labels are the same as the
+            inputs with the padding tokens ignored (by setting them to -100). Otherwise, the labels are -100 for
+            non-masked tokens and the value to predict for the masked token.
+        mlm_probability (:obj:`float`, `optional`, defaults to 0.15):
+            The probability with which to (randomly) mask tokens in the input, when :obj:`mlm` is set to :obj:`True`.
+        pad_to_multiple_of (:obj:`int`, `optional`):
+            If set will pad the sequence to a multiple of the provided value.
+
+    .. note::
+
+        For best performance, this data collator should be used with a dataset having items that are dictionaries or
+        BatchEncoding, with the :obj:`"special_tokens_mask"` key, as returned by a
+        :class:`~transformers.PreTrainedTokenizer` or a :class:`~transformers.PreTrainedTokenizerFast` with the
+        argument :obj:`return_special_tokens_mask=True`.
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    mlm: bool = True
+    mlm_probability: float = 0.15
+    pad_to_multiple_of: Optional[int] = None
+    n_gram:int = 3
+
+
+    def mask_tokens(
+        self, inputs: torch.Tensor, special_tokens_mask: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        if special_tokens_mask is None:
+            special_tokens_mask = [
+                self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+            ]
+            special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+        else:
+            special_tokens_mask = special_tokens_mask.bool()
+
+
+        probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        # blockify the masked tokens
+        for batch_idx in range(labels.shape[0]):
+            low, up = 0, labels.shape[1]
+            mask_sum = masked_indices[batch_idx].sum()
+            num_blocs = (mask_sum + self.n_gram - 1) // self.n_gram
+            if num_blocs == 0:
+                continue
+            bloc_indices = torch.randint(low, up, (num_blocs,))
+            masked_indices[batch_idx] = False
+            for bloc_idx in bloc_indices:
+                low_bloc_idx = max(low, bloc_idx - self.n_gram // 2)
+                up_bloc_idx = min(up, bloc_idx + self.n_gram // 2)
+                masked_indices[batch_idx][low_bloc_idx: (up_bloc_idx + 1)] = False
+            
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        inputs[indices_replaced] = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(len(self.tokenizer), labels.shape, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
